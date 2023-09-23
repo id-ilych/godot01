565e4509fb0d188f2f101219d59cf65db1818922
Ilya Denisov <id.ilych@gmail.com>
2023-09-23 23:31:24 +0300

    disable selected class button

--- behaviors/select_class_button.gd
+++ behaviors/select_class_button.gd
@@ -5,6 +5,12 @@ class_name SelectClassButton
 @export var target: CharacterClassReference
 var character_class: CharacterClass
 
+func _ready():
+	if target:
+		target.changed.connect(refresh)
+	
+	refresh()
+
 func _on_pressed():
 	if target and character_class:
 		target.set_value(character_class)
@@ -12,3 +18,8 @@ func _on_pressed():
 func set_character_class(new_class):
 	character_class = new_class
 	text = new_class.name
+	refresh()
+
+func refresh():
+	var selected = target and target.value == character_class
+	disabled = selected
